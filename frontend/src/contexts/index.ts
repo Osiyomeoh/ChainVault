2c21f8a8b215178e50b98909ac7428eb76bad349
Osiyomeoh <samuelaleonomoh5@gmail.com>
2025-08-27 13:15:19 +0100

    started working on a fresh frontend, with seeting the environment config

--- frontend/src/contexts/index.ts
+++
@@ -1,3 +0,0 @@
-export { AuthProvider, useAuth } from './AuthContext';
-export { VaultProvider, useVault } from './VaultContext';
-export { SBTCVaultProvider, useSBTCVault } from './SBTCVaultContext';
